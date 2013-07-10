7eb90873a7def67b7b850c887c07354da4e05b80
jlmondelo <joseluis@mondelo.es>
2013-07-10 23:12:56 +0000

    grader.js updated to get url

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -61,11 +62,30 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var getUrl = function(apiurl) {
+    restler.get(apiurl).on('complete', function(result){
+	if(result instanceof Error) {
+	    console.log('Error: '+ result.message);
+	    process.exit(1);
+	} else {
+	    program.file = 'temp.txt';
+	    fs.writeFileSync(program.file,result.toString());
+	}
+    })
+};
+
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_file>', 'Link to a url')
         .parse(process.argv);
+    if(program.url) {
+	getUrl(program.url)
+    }else{
+	assertFileExists(program.file);
+    }	
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
