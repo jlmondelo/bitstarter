c6298b43d75feba0a702c1b32e688ddfc7d42631
jlmondelo <joseluis@mondelo.es>
2013-07-10 18:50:49 +0000

    bla

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = new Buffer(fs.readFile("index.html",'utf-8'),'utf-8');
+  var buffer = new Buffer(fs.readFileSync("index.html",'utf-8'),'utf-8');
    response.send(buffer.toString());
 });
 
